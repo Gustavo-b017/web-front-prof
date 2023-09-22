af3c4ff894c0799d66f678bbe486e4ef932095b5
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-09-22 11:08:07 -0300

    adicionando arquivo de variavel

---
+++ variavel.js
@@ -0,0 +1,10 @@
+// cria uma variavel com valor indefinido
+let nome;
+
+// cria uma variavel vazia
+let disciplina = null;
+
+// cria uma variavel com valor
+let texto = "Este é um exemplo";
+
+console.log(texto);
